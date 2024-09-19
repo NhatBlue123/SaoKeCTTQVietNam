1920e28b5845c01e0631d0a3ba507e7d279a3017
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-09-19 12:36:34 +0700

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { MantineProvider } from '@mantine/core';
 
 import Home from './components/home';
 import BarChart1 from './components/Matine/BarChart';
-import Main from './components/main';
 export default function App() {
   return (
     <>
       <MantineProvider>
           <Home></Home>
-          <Main></Main>
       </MantineProvider>
     </>
   )
