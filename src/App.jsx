ccd7ba7a5fc09979be1faae4951d79c0e323f0a5
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-09-28 19:21:45 +0700

    Refactor App component to include Header and Main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { MantineProvider } from '@mantine/core';
-
-import Home from './components/home.jsx';
+import { MantineProvider } from "@mantine/core";
+import Header from "./components/header.jsx";
+import Main from "./components/Main.jsx";
+// import Home from './components/home.jsx';
 export default function App() {
   return (
     <>
       <MantineProvider>
-          <Home></Home>
+        <Header></Header>
+        <Main></Main>
       </MantineProvider>
     </>
-  )
+  );
 }
