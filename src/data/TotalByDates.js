99a3c8b7bd8d64bdb093c94e3298c490a15b0d41
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-10-20 19:13:30 +0700

    fix VCB and add total by dates VCB

--- src/data/TotalByDates.js
+++ src/data/TotalByDates.js
@@ -1,28 +1,10 @@
-export const dataVCB110 = [
-  { name: "1/9", VND: 149000 },
-  { name: "2/9", VND: 572000 },
-  { name: "3/9", VND: 1111000 },
-  { name: "4/9", VND: 1687000 },
-  { name: "5/9", VND: 202888 },
-  { name: "6/9", VND: 707000 },
-  { name: "7/9", VND: 1865752 },
-  { name: "8/9", VND: 65920200 },
-  { name: "9/9", VND: 1085663566 },
-  { name: "10/9", VND: 35418614300 },
-];
+
 export const dataVCB1012 = [
   { name: "10/9", VND: 360955656 },
   { name: "11/9", VND: 4453242994 },
   { name: "12/9", VND: 969500581 },
 ];
 
-export const dataVCB119 = [{ name: "11/9", VND: 13620845912 }];
-export const dataAgibank913 = [
-  { name: "9/9", VND: 500000 },
-  { name: "10/9", VND: 13476139273 },
-  { name: "11/9", VND: 14157648898 },
-  { name: "12/9", VND: 15803696026 },
-];
 
 export const BIDV = [
   { name: "1/9", VND: 150000 },
@@ -60,5 +42,21 @@ export const VietTinBank = [
   { name: "15/9", VND: 8913658777 },
   { name: "16/9", VND: 23529934669 },
   { name: "17/9", VND: 16238524680 }
+];
+export const VCB = [
+  { name: "1/9", VND: 149000 },
+  { name: "2/9", VND: 572000 },
+  { name: "3/9", VND: 1111000 },
+  { name: "4/9", VND: 1687000 },
+  { name: "5/9", VND: 202888 },
+  { name: "6/9", VND: 707000 },
+  { name: "7/9", VND: 1865752 },
+  { name: "8/9", VND: 65920200 },
+  { name: "9/9", VND: 7868744495 },
+  { name: "10/9", VND: 126308988811 },
+  { name: "11/9", VND: 208849755065 },
+  { name: "12/9", VND: 207878316893 },
+  { name: "13/9", VND: 275811710532 },
+  { name: "14/9", VND: 112679659428 },
 ];
 // export default {data,data1012,dataBIDV112,dataVTB1012}
