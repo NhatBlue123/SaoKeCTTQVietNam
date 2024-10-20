889fa9f0a88c7440bcb11578e8fc84e5dfe25c70
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-10-20 20:11:20 +0700

    Refactor Header component to add marquee effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,15 @@ const Header = () => {
   return (
     <>
       <div className="flex justify-center items-center mb-10">
-        <div className="">
+        <div className=""></div>
+        <marquee behavior="" direction="">
           <h2 className="font-bold text-4xl ">
             Sao Kê Mùa Bão Lũ 🔍 By{" "}
             <a href="https://github.com/NhatBlue123" target="_blank">
               Nguyễn Thanh Nhật
             </a>
           </h2>
-        </div>
+        </marquee>
       </div>
     </>
   );
